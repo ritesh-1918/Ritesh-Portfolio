26a038e8cb6ccfc380c470c28ab3def0b60343c5
B. Ritesh <bonthalamadhavi1@gmail.com>
2025-03-17 21:52:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Script is working!");
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+    duration: 1000,
+    once: true,
+    offset: 120
+});
+
+// Smooth scroll for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
